47868debd1b23ce304b28f5a3f1f7204c9239610
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2023-11-12 23:17:06 +0200

    background basic animation

---
+++ js/background.js
@@ -0,0 +1,78 @@
+class Layer {
+  constructor(game, width, height, image, speedCoef) {
+    this.game = game
+    this.width = width
+    this.height = height
+    this.x = 0
+    this.y = 0
+    this.image = image
+    this.speedCoef = speedCoef
+  }
+  update() {
+    if (this.x < -this.width) this.x = 0
+    else {
+      this.x -= 1 * this.speedCoef
+    }
+  }
+  draw(context) {
+    context.drawImage(this.image, this.x, this.y, this.width, this.height)
+    context.drawImage(
+      this.image,
+      this.x + this.width,
+      this.y,
+      this.width,
+      this.height
+    )
+  }
+}
+
+export class Background {
+  constructor(game) {
+    this.game = game
+    this.width = 1667
+    this.height = 500
+    this.imageLayer1 = layer1
+    this.imageLayer2 = layer2
+    this.imageLayer3 = layer3
+    this.imageLayer4 = layer4
+    this.layer1 = new Layer(
+      this.game,
+      this.width,
+      this.height,
+      this.imageLayer1,
+      0
+    )
+    this.layer2 = new Layer(
+      this.game,
+      this.width,
+      this.height,
+      this.imageLayer2,
+      0.4
+    )
+    this.layer3 = new Layer(
+      this.game,
+      this.width,
+      this.height,
+      this.imageLayer3,
+      0.7
+    )
+    this.layer4 = new Layer(
+      this.game,
+      this.width,
+      this.height,
+      this.imageLayer4,
+      1
+    )
+    this.bgLayers = [this.layer1, this.layer2, this.layer3, this.layer4]
+  }
+  update() {
+    this.bgLayers.forEach((layer) => {
+      layer.update()
+    })
+  }
+  draw(context) {
+    this.bgLayers.forEach((layer) => {
+      layer.draw(context)
+    })
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import { Background } from './background.js'
 import { InputHandler } from './inputHandler.js'
 import { Player } from './player.js'
 
@@ -5,31 +6,38 @@ window.addEventListener('load', function (e) {
   const canvas = this.document.getElementById('canvas')
   const ctx = canvas.getContext('2d')
   canvas.width = 1000
-  canvas.height = 600
+  canvas.height = 500
 
   class Game {
     constructor(width, height) {
       this.width = width
       this.height = height
       this.groundHeight = 70
+      this.speed = 0
       this.player = new Player(this)
       this.input = new InputHandler(this)
+      this.background = new Background(this)
     }
-    update() {
-      this.player.update(this.input)
+    update(deltaTime) {
+      this.background.update()
+      this.player.update(this.input, deltaTime)
     }
     draw(context) {
+      this.background.draw(context)
       this.player.draw(context)
     }
   }
 
   const game = new Game(canvas.width, canvas.height)
+  let lastTime = 0
 
-  function animate() {
+  function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime
+    lastTime = timeStamp
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    game.update()
+    game.update(deltaTime)
     game.draw(ctx)
     requestAnimationFrame(animate)
   }
-  animate()
+  animate(0)
 })
